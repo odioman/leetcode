9dadc00e0f295548a083b2a126f005a56869d8d9
Thomas Koshy <ttkoshy@gmail.com>
2023-08-02 16:32:33 -0400

    kenson and mine solution for 1331

--- main.js
+++ main.js
@@ -1476,3 +1476,42 @@ for (let i = nums.length - 1; i >= 0; i--) {
 
     return false;
 };
+
+/* 1331. Rank of Transform an Array */
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+ var arrayRankTransform = function(arr) {
+    //declare final array
+    const finalArr = []; 
+    //copy array and sort
+    const sortedArr = [...arr].sort((a,b) => {
+        return a - b
+    }); 
+    //create a variable to hold index set default to 1
+    let rank = 1;
+    //create a Map to store the ranks of each number
+    const numberToRanks = new Map();
+    //iterate over sorted array
+     for (const number of sortedArr) {
+         //check to see if current element exists in Map
+         if (numberToRanks.has(number)) {
+             //continue 
+             continue   
+         }
+         numberToRanks.set(number, rank); 
+         rank++;
+         //if it doesn't exist in Map, add current number and its rank to the Map
+         //increment the variable
+      }
+     //iterate over arr 
+     for (const number of arr) {
+         //Map.get to receive rank of number
+         const getRank = numberToRanks.get(number)
+         //push to final array
+         finalArr.push (getRank)    
+     }
+     //return final array 
+     return finalArr 
+ };
